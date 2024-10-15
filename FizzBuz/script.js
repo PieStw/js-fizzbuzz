7bf300c37568047ec8641db75324719257a88928
PieStw <piero.stalteri@icloud.com>
2024-10-15 13:13:12 +0200

    aggiunto stampa variabile

--- FizzBuz/script.js
+++ FizzBuz/script.js
@@ -9,12 +9,14 @@ for(i = 1; i < 101; i++){
 
     //primo controllo
     if(i % 3 == 0){
-
+        stampa += "Fizz"
     }
 
     //secondo controllo
     if(i % 5 == 0){
-
+        stampa += "Buzz"
     }
 
+    console.log(stampa)
+    stampa = "";
 }
