79dbb45274dfb60998e6673d57247eb2939ae462
PieStw <piero.stalteri@icloud.com>
2024-10-15 13:10:00 +0200

    primo controllo

--- FizzBuz/script.js
+++ FizzBuz/script.js
@@ -1,10 +1,13 @@
 
 
 // creo un ciclo per selezionare 100 elementi
-
-for(i = 0; i < 100; i++){
-
+for(i = 1; i < 101; i++){
 
 
-    
+    //primo controllo
+    if(i % 3 == 0){
+        
+    }
+
+
 }
