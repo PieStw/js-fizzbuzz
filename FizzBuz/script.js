db3abf2c94bd7072509c212c5b7d4bed4f9c6bcf
PieStw <piero.stalteri@icloud.com>
2024-10-15 13:14:23 +0200

    aggiunto commento

--- FizzBuz/script.js
+++ FizzBuz/script.js
@@ -9,14 +9,18 @@ for(i = 1; i < 101; i++){
 
     //primo controllo
     if(i % 3 == 0){
+        //modifico la variabile
         stampa += "Fizz"
     }
 
     //secondo controllo
     if(i % 5 == 0){
+        //modifico la variabile
         stampa += "Buzz"
     }
 
+    // stampo la variabile
     console.log(stampa)
+    // resetto la variabile
     stampa = "";
 }
